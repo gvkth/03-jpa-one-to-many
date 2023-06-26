f82ac7bc0e08b0fc1bea73af72fe94ba2cae3e5a
cuongvk <cuongvk.hcm@vnpt.vn>
2023-06-27 00:18:37 +0700

    Mapping ORM: 1-N : fetch types EAGER vs LAZY

--- src/main/java/com/luv2code/cruddemo/entity/Instructor.java
+++ src/main/java/com/luv2code/cruddemo/entity/Instructor.java
@@ -96,6 +96,7 @@ public class Instructor {
     }
 
     @OneToMany(mappedBy = "instructor",
+            fetch = FetchType.EAGER,
             cascade = {CascadeType.PERSIST, CascadeType.MERGE,CascadeType.DETACH,CascadeType.REFRESH}
     )
     private List<Course> courses;
